bdadc4a2788610ae19fc4bf7e5498fa20cc9c796
dongrerohan421 <dongrerohan421@gmail.com>
2020-08-28 11:12:09 -0400

    create cart service to add product to cart

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
+import { CartService } from './../src/app/cart.service';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { products } from '../products';
 
 @Component({
@@ -8,17 +9,21 @@ import { products } from '../products';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-
   product;
 
   constructor(
-    private route:ActivatedRoute
-  ) { }
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      this.product = products[+params.get('productId')]
+    this.route.paramMap.subscribe((params) => {
+      this.product = products[+params.get('productId')];
     });
   }
 
+  addToCart = (product) => {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  };
 }
---
+++ src/app/src/app/cart.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  items = [];
+  constructor() {}
+
+  // addToCart() method appends a product to an array of items.
+  addToCart = (product) => {
+    return this.items.push(product);
+  };
+  // The getItems() method collects the items users add to the cart and returns each item with its associated quantity.
+  getItems = () => {
+    return this.items;
+  };
+  //The clearCart() method returns an empty array of items.
+  clearCart = () => {
+    this.items = [];
+    return this.items;
+  };
+}
